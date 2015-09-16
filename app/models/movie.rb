69191172d7085d52d86d7c4487402bbe6ba189a4
Fillipe Feitosa <saasbook@saasbook.(none)>
2015-09-15 22:26:31 -0300

    Movie method to return array of ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
+
+  #necessário para a construção da view, ordenação futura e etc
+  def self.all_ratings
+      ["G", "PG", "PG-13", "R", "NC-17"]
+  end
+
 end
